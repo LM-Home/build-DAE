8c6ad6f46f60984b0f36694f471b9ce0d1e16298
LM-Home <90747587+LM-Home@users.noreply.github.com>
2025-08-11 09:09:47 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 检查必要的环境变量
 if [[ -z "$NGROK_TOKEN" ]]; then
 	echo "Please set 'NGROK_TOKEN'"
 	exit 2
@@ -10,17 +11,21 @@ if [[ -z "$USER_PASS" ]]; then
 	exit 3
 fi
 
-curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc
-sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-sudo echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main"
-sudo tee /etc/apt/sources.list.d/ngrok.list
-sudo apt update
-sudo apt install ngrok
-rm -f .ngrok.log
-./ngrok authtoken "$NGROK_TOKEN"
-./ngrok tcp 22 --log ".ngrok.log" &
+# 安装 ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" | sudo tee /etc/apt/sources.list.d/ngrok.list >/dev/null
+sudo apt update -y
+sudo apt install ngrok -y
 
+# 配置 ngrok 并启动
+rm -f .ngrok.log
+ngrok authtoken "$NGROK_TOKEN"
+ngrok tcp 22 --log ".ngrok.log" &
+
+# 等待 ngrok 启动
 sleep 10
+
+# 检查是否有错误
 HAS_ERRORS=$(grep "command failed" <.ngrok.log)
 
 if [[ -z "$HAS_ERRORS" ]]; then
